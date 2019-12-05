fdd183cd8ef9ed246c968a6ec2ef0ecd09c72278
marcelorai <marcelorai1@hotmail.com>
2019-12-05 02:06:32 -0300

    Implementação dos links da página

--- caicolanches/cadastroEmp/urls.py
+++ caicolanches/cadastroEmp/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from cadastroEmp.views import IndexTemplateView, EstabelecimentoListView, EstabelecimentoUpdateView, EstabelecimentoCreateView, \
+    EstabelecimentoDeleteView
 
 from . import views
 
+app_name = 'cadastroEmp'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexTemplateView.as_view(), name="index"),
+
+    path('estabelecimento/cadastrar', EstabelecimentoCreateView.as_view(), name="cadastra_estabelecimento"),
+
+    path('estabelecimentos/', EstabelecimentoListView.as_view(), name="lista_estabelecimentos"),
+
+    path('estabelecimento/<pk>', EstabelecimentoUpdateView.as_view(), name="atualiza_estabelecimento"),
+
+    path('estabelecimento/excluir/<pk>', EstabelecimentoDeleteView.as_view(), name="deleta_estabelecimento"),
 ]
