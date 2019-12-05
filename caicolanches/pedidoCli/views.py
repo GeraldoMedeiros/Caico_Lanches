3fb187b48e4f141a2c78c09e37960907b41e3078
marcelorai <marcelorai1@hotmail.com>
2019-12-05 01:56:37 -0300

    Formatação da frase de aviso

--- caicolanches/pedidoCli/views.py
+++ caicolanches/pedidoCli/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Página do cliente em desenvolvimento")
+    return HttpResponse("Página do cliente em desenvolvimento!")
 
 
 #class ReceiverForm(forms.ModelForm):
