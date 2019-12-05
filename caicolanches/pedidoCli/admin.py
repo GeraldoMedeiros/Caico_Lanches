05713378355015574b63cbefec64362f44117b71
marcelorai <marcelorai1@hotmail.com>
2019-12-05 01:58:21 -0300

    chamadas criadas para o site admin

--- caicolanches/pedidoCli/admin.py
+++ caicolanches/pedidoCli/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cliente
+from .models import Endereco
+
+admin.site.register(Cliente)
+admin.site.register(Endereco)
