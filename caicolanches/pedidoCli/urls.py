fc29da8c4933dd2e649ac518d3480058342c0b87
Geraldo Medeiros <geraldo_sjs@hotmail.com>
2019-12-05 13:26:04 -0300

    outras templates ainda vazias

--- caicolanches/pedidoCli/urls.py
+++ caicolanches/pedidoCli/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from pedidoCli.views import IndexTemplateView
+#from pedidoCli.views import IndexTemplateView
 from . import views
 
 app_name = 'pedidoCli'
 urlpatterns = [
-    path('', IndexTemplateView.as_view(), name="index"),
+    path('', views.index, name="index"),
 ]
--- caicolanches/pedidoCli/views.py
+++ caicolanches/pedidoCli/views.py
@@ -3,15 +3,9 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView
 # Create your views here.
 
-<<<<<<< HEAD
 
-class IndexTemplateView(TemplateView):
-    template_name = "pedidoCli/index.html"
-=======
 def index(request):
-    return HttpResponse("Página do cliente em desenvolvimento!")
->>>>>>> 52cee33bf69c077b99494297b713d94d4d86fe87
-
+    return HttpResponse("Em desenvolvimento")
 
 # class ReceiverForm(forms.ModelForm):
 #    phone_number = forms.RegexField(regex=r'\d{10,11}$', error_message = ("Entre com um número no formato: 'xx987654321'"))
