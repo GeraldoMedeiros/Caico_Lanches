54ee205e52012b31dced2f676a3c8afece7a9328
Geraldo Medeiros <geraldo_sjs@hotmail.com>
2019-12-12 17:14:43 -0300

    Admin

--- caicolanches/pedidoCli/admin.py
+++ caicolanches/pedidoCli/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Cliente
 from .models import Endereco
+from .models import Produto
 
 admin.site.register(Cliente)
 admin.site.register(Endereco)
+admin.site.register(Produto)
